71fc565ba41c65f528ad65e24536234b10ab1e8e
chandrashekharbhaupande <chpande55@gmail.com>
2026-02-16 22:59:45 +0530

    service added

--- src/main/java/com/jarvis/callexternalapi/services/UsingRestTemplate.java
+++ src/main/java/com/jarvis/callexternalapi/services/UsingRestTemplate.java
@@ -1,2 +1,25 @@
-package com.jarvis.callexternalapi.services;public class UsingRestTemplate {
+package com.jarvis.callexternalapi.services;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class UsingRestTemplate {
+
+    @Value("${external.service.posturl}")
+    String posturl;
+
+    public void callExternalServiceUsingRestTemplate(){
+
+        RestTemplate restTemplate = new RestTemplate();
+        // null because service does not have any payload or headers
+        // String.class is Response type
+        ResponseEntity<String> response = restTemplate.exchange(posturl, HttpMethod.GET,null, String.class);
+        System.out.println(response.getBody());
+
+    }
 }
