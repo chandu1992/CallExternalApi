14cf1b8870b64789e50e55da0bc10bae5be9598d
chandrashekharbhaupande <chpande55@gmail.com>
2026-02-16 22:32:22 +0530

    external api call

---
+++ src/main/java/com/jarvis/callexternalapi/CallexternalapiApplication.java
@@ -0,0 +1,13 @@
+package com.jarvis.callexternalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallexternalapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallexternalapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jarvis/callexternalapi/CallexternalapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.callexternalapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallexternalapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
